693dfbb3a0dd6c5bca1f2568fe55dc0372c9603c
DomiurgM <m.system@gmail.com>
2025-01-05 21:19:21 +0200

    удален лишний файл

--- test.cmd
+++
@@ -1,2 +0,0 @@
-file_content=$(cat test.txt.b64)
-curl -X POST -H "Content-Type: application/json" -d '{"FileBoby": "'"$file_content"'", "FileName": "my_test_file.txt"}' http://localhost:8080/SaveFileToStorage
